1d1002bcd853e863de4595bc6dc335b31116821f
Joao-Okabe <joao.okabe.berghem@gmail.com>
2025-09-10 15:24:16 -0300

    merjação suprema

--- SCRIPT/index.js
+++ SCRIPT/index.js
@@ -2,25 +2,32 @@ const telaInicial = document.getElementById("tela-inicial");
 const loginAlunoDiv = document.getElementById("login-aluno");
 const loginFuncDiv = document.getElementById("login-funcionario");
 
-document.getElementById("btnAluno").addEventListener("click", function() {
-    telaInicial.style.display = "none";
-    loginAlunoDiv.style.display = "block";
-});
-
-document.getElementById("btnFuncionario").addEventListener("click", function() {
-    telaInicial.style.display = "none";
-    loginFuncDiv.style.display = "block";
-});
-
-function voltar() {
-    loginAlunoDiv.style.display = "none";
-    loginFuncDiv.style.display = "none";
-    telaInicial.style.display = "block";
-}
-
 function loginAluno() {
-    const rm = document.getElementById("rm").value.trim();
-    const senha = document.getElementById("senha").value.trim();
+    const rm = document.getElementById("rmInput").value.trim();
+    const senha = document.getElementById("senhaInput").value.trim();
+
+    $.ajax({
+        url: '../PHP/loginAluno.php', // URL do PHP
+        type: 'POST',
+        data: {
+            rm: rm,
+            senha: senha
+        },
+        dataType: 'json',
+        success: function(response) {
+            console.log(response); // Exibe a resposta real do servidor
+            if (response.status == 'success') {
+                alert('Usuário logado como ' + response.rm);
+                window.location.href = '../HTML/AreaAluno.html';
+            } else {
+                alert(response.message || 'Erro ao tentar fazer login.');
+            }
+        },
+        error: function(xhr, error) {
+            console.error('Erro na requisição AJAX:', error);
+            console.log(xhr.responseText); // Exibe a resposta completa da requisição para depuração
+        }
+    });
 
     $.ajax({
         url: '../PHP/loginAluno.php', // URL do PHP
@@ -47,8 +54,8 @@ function loginAluno() {
 }
 
 function loginFuncionario() {
-    const CPF = document.getElementById("CPF").value.trim();
-    const Senha = document.getElementById("SenhaFunc").value.trim();
+    const CPF = document.getElementById("rmInput").value.trim();
+    const Senha = document.getElementById("senhaInput").value.trim();
     
     $.ajax({
         url: "../PHP/loginProf.php", // URL para o PHP do funcionário
